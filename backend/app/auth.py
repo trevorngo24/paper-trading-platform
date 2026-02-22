16cb8800bc7be8011740317673cce2af53a56c78
Trevor Ngo <trevorngo14@gmail.com>
2026-02-22 00:04:33 -0800

    Fixed database organization & added password hashing

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,3 +1,9 @@
 from passlib.context import CryptContext
 
-pwd = 
+bycrypt_context = CryptContext(schemes=['bycrypt'], deprecated= 'auto') # used to  password hashing and unhashing
+
+def hash_password(password: str) -> str:
+    return bycrypt_context.hash(password) # turn a plain pw to a secure hashed pw 
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bycrypt_context.verify(plain_password, hash_password) # check if the login password matches the stored hash 
