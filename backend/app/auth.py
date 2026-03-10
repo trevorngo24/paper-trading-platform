d4cd2445d70e58ce458675e83effbe7b6248e6b4
Trevor Ngo <trevorngo14@gmail.com>
2026-03-10 14:52:11 -0700

    Implement JWT authentication with login and  token generation

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,4 +1,18 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from sqlalchemy.orm import Session
+
+from .database import get_db
+from .models import User
+
+SECRET_KEY = "qwertyuiop12wu3pj7fhgf8j9401cdh74gfh5fd28"
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # used to password hashing and unhashing
 
@@ -6,4 +20,58 @@ def hash_password(password: str) -> str:
     return pwd_context.hash(password) # turn a plain pw to a secure hashed pw 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password) # check if the login password matches the stored hash 
+    return pwd_context.verify(plain_password, hashed_password) # check if the login password matches the stored hash
+
+# Function to create a JWT token when the user logs in / essentially we are creating an token that contains the user(sub) and its expiration
+def create_access_token(data:dict, expires_delta: timedelta | None = None) -> str: 
+    to_encode = data.copy() # copy the data dic so we don't modify the orginal, data = sub: trevor
+    expire = datetime.now(timezone.utc) + ( # create expiration date for the token for user safety 
+        expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES )
+    )
+    to_encode.update({"exp": expire}) #exp: 2026-03-19 19:30
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def authenticate_user(db: Session, email:str, password: str):
+    user = get_user_by_email(db, email)
+
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    
+    return user
+
+# Checks the JWT Token and returns the logged-in user, checking if a request is coming from logged in user
+def get_current_user(
+        token: str = Depends(oauth2_scheme), # fastapi gets the token from auth header
+        db: Session = Depends(get_db) # database session dependency, gives access to db
+): # flow: login -> token gen -> protected route req -> decode token  -> extracts user id (sub) -> checks db for user -> return authenticated user
+    
+      # If anything goes wrong we throw this error
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+
+        # This tells the client authentication failed
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # Decode the JWT token using the secret key
+        user_id = payload.get("sub") # Extract the "sub" field from the token payload
+        
+        if user_id is None:# If the token does not contain a user id, reject it
+            raise credentials_exception
+        user = db.query(User).filter(User.id == int(user_id)).first()
+        if user is None: # If the user no longer exists, reject token
+            raise credentials_exception
+        return user
+    
+    except (InvalidTokenError, ValueError): # If the token is invalid or malformed
+        raise credentials_exception  # Raise the authentication error
+    
+    
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from app.database import Base, engine
 from app import models
 from app.session import session_local
-from app.auth import hash_password, verify_password
+from app.auth import hash_password, verify_password, create_access_token, get_current_user
 from app.models import User
 from app.schemas import UserCreate, UserLogin
 
@@ -15,6 +15,13 @@ app = FastAPI(title="Paper Trading Platform API")
 def register(user: UserCreate):
     db = session_local() # we want to run the session in order to makes changes
     try:
+        existing_user = db.query(User).filter(User.username == user.username).first()
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Username already exists")
+        existing_email = db.query(User).filter(User.email == user.email).first()
+        if existing_email:
+            raise HTTPException(status_code=400, detail="Email already exists")
+
         hashed_pw = hash_password(user.password) # hash the plain text pw 
         newUser = User( # we create an database row and fill it in using user input info
             username=user.username,
@@ -23,7 +30,12 @@ def register(user: UserCreate):
         )
         db.add(newUser) # add new user to db
         db.commit() # commit it to the db
-        return {"message": "User created", "username": user.username} # return this text 
+        db.refresh(newUser)
+        return {
+            "message": "User created",
+            "username": newUser.username,
+            "email": newUser.email,
+        }
     finally:
         db.close()
 
@@ -31,15 +43,29 @@ def register(user: UserCreate):
 def login(user: UserLogin):
     db = session_local()
     try:
-        user = db.query(User).filter(User.username == user.username).first() # go inside the db and look the user table and find the row where the username matches the input username and give me the first match
-        if not user: # if not in there return error message
-            return{"error": "User Not Found"}
-        if not verify_password(user.password, user.hashed_password): 
-            return{"error": "Invalid Password"}
-        return {"message": "Login Sucessful", "username": user.username}
-    
+        db_user = db.query(User).filter(User.username == user.username).first() # go inside the db and look the user table and find the row where the username matches the input username and give me the first match
+        if not db_user: # if not in there return error message
+            raise HTTPException(status_code=404, detail="User not found")
+
+        if not verify_password(user.password, db_user.hashed_password): 
+            raise HTTPException(status_code=401, detail="Invalid password")
+        access_token = create_access_token(data={"sub": str(db_user.id)})
+        return {
+            "message": "Login successful",
+            "access_token": access_token,
+            "token_type": "bearer",
+        }    
     finally:
         db.close()
+
+@app.get("/me")
+def read_current_user(current_user: User = Depends(get_current_user)): # get_current_user from auth.py
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email,
+
+    }
 
     
 
