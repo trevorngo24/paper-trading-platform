cf71bc372b04b40e8c78b7629a82cab89976daf5
Trevor Ngo <trevorngo14@gmail.com>
2026-03-24 15:35:06 -0700

    Added Porfolio model and implemented trade buy endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from app.database import Base, engine
 from app import models
 from app.session import session_local
 from app.auth import hash_password, verify_password, create_access_token, get_current_user
-from app.models import User, Portfolio
+from app.models import User, Portfolio, Trade
 from app.schemas import UserCreate, UserLogin
 
 Base.metadata.create_all(bind=engine) # create the table if table doesnt exist 
@@ -50,7 +50,7 @@ def register(user: UserCreate):
 def login(user: UserLogin):
     db = session_local()
     try:
-        db_user = db.query(User).filter(User.email == user.email).first() # go inside the db and look the user table and find the row where the username matches the input username and give me the first match
+        db_user = db.query(User).filter(User.username == user.username).first() # go inside the db and look the user table and find the row where the username matches the input username and give me the first match
         if not db_user: # if not in there return error message
             raise HTTPException(status_code=404, detail="User not found")
 
@@ -76,19 +76,62 @@ def read_current_user(current_user: User = Depends(get_current_user)): # get_cur
 
 @app.get("/portfolio")
 def get_portfolio(current_user: User = Depends(get_current_user)):
-    db = session_local # we want to run the session in order to makes changes
+    db = session_local() # we want to run the session in order to makes changes
     try:
         portfolio = db.query(Portfolio).filter(Portfolio.user_id == current_user.id).first() # find the portfolio associated to the user
         
         if not portfolio:
             raise HTTPException(status_code=404, detail="Portfolio not found")
         return {
-            "user.id": current_user.id,
+            "user_id": current_user.id,
             "username": current_user.username,
             "cash_balance": portfolio.cash_balance
         }
     finally:
         db.close()
+
+@app.post("/trade/buy") # post bc the user is sending data to the serber to create a trade, and that trade is stored in the db
+def buy_stock(symbol: str, quantity: int, current_user: User = Depends(get_current_user)):
+    db = session_local()
+    try:
+        portfolio = db.query(Portfolio).filter(Portfolio.user_id == current_user.id).first()
+        
+        if not portfolio:
+            raise HTTPException(status_code=404, detail="Portfolio not found")
+        if quantity <= 0:
+            raise HTTPException(status_code=400, detail="Quantity must be greater than 0")
+        
+        price = 100.0 # this is just an placeholder price, later we will change this using an external API to get real stock prices
+        total_cost = price * quantity
+
+        if portfolio.cash_balance < total_cost:
+            raise HTTPException(status_code=400, detail="Insufficient Funds")
+        
+        portfolio.cash_balance -= total_cost
+
+        trade = Trade( # we create an row to store this into the Trade table
+            user_id=current_user.id,
+            symbol=symbol.upper(),
+            quantity=quantity,
+            price=price,
+            trade_type="buy"
+        )
+        db.add(trade)
+        db.commit()
+
+        return {
+            "message": "Stock purchased",
+            "symbol": symbol.upper(),
+            "quantity": quantity,
+            "price": price,
+            "total_cost": total_cost,
+            "remaining_balance": portfolio.cash_balance
+        }
+    finally:
+        db.close()
+
+
+
 
 
     
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, declarative_base
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, declarative_base, relationship
 from .database import Base
+from sqlalchemy import ForeignKey
 
 class User(Base): # creates an database called users
     __tablename__ = "users" 
@@ -8,5 +9,32 @@ class User(Base): # creates an database called users
     email: Mapped[str] = mapped_column(unique = True)
     hashed_password: Mapped[str]
 
+    portfolio : Mapped["Portfolio"] = relationship(back_populates="user") # user to portfolio
+
     def __repr__(self) -> str:
         return f"<User(id={self.id}, username = {self.username}, email = {self.email})>"
+    
+class Portfolio(Base): # create table named portfolios
+        __tablename__ = "portfolios"
+
+        id: Mapped[int] = mapped_column(primary_key=True) 
+        user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True) # forgein key, every portfolio belongs to an user
+        cash_balance: Mapped[float] = mapped_column(default=10000.0) # every will start with an $10,000 balance
+
+        user: Mapped["User"] = relationship(back_populates="portfolio") # creates the bridge from porfolio to user
+
+        def __repr__(self) -> str:
+            return f"<Portfolio(id={self.id}, user_id={self.user_id}, cash_balance={self.cash_balance})>"
+
+class Trade(Base):
+     __tablename__ = "trades"
+
+     id: Mapped[int] = mapped_column(primary_key=True)
+     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+     symbol: Mapped[str] # stock name such as tsla, aapl
+     quantity: Mapped[int] # how many shares u want to buy
+     price: Mapped[float]
+     trade_type: Mapped[str] # bought or sold
+
+     def __repr__(self) -> str:
+          return f"<Trade(id={self.id}, user_id={self.user_id}, symbol={self.symbol}, quantity={self.quantity}, price={self.price}, trade_type={self.trade_type})>"
